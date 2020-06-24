2887df317e07276832dd8966291f0afb55348ce7
Israel Oliveira <raloliver.com@gmail.com>
2020-06-24 23:18:15 +0100

    Before setup route

--- src/components/shared/panel/Panel.vue
+++ src/components/shared/panel/Panel.vue
@@ -3,56 +3,56 @@
     <h2 class="panel-header" @dblclick="isVisible = !isVisible">{{ title }}</h2>
     <transition name="fade">
       <div class="panel-content" v-show="isVisible">
-        <slot> </slot>
+        <slot></slot>
       </div>
     </transition>
   </div>
 </template>
 
 <script>
-  export default {
-    props: ['title'],
-    data() {
-      return {
-        isVisible: true
-      }
-    }
-  };
+export default {
+  props: ["title"],
+  data() {
+    return {
+      isVisible: true
+    };
+  }
+};
 </script>
 
-<style lang="scss" scoped>
-  .panel {
-    padding: 0 auto;
-    border: solid 2px grey;
-    display: inline-block;
-    margin: 5px;
-    box-shadow: 5px 5px 10px grey;
-    width: 200px;
-    height: 100%;
-    vertical-align: top;
-    text-align: center;
-  }
+<style scoped>
+.panel {
+  padding: 0 auto;
+  border: solid 2px grey;
+  display: inline-block;
+  margin: 5px;
+  box-shadow: 5px 5px 10px grey;
+  width: 200px;
+  height: 100%;
+  vertical-align: top;
+  text-align: center;
+}
 
-  .panel-header {
-    text-align: center;
-    border: solid 2px;
-    background: lightblue;
-    margin: 0 0 15px 0;
-    padding: 10px;
-    text-transform: uppercase;
-  }
+.panel-header {
+  text-align: center;
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
 
-  * {
-    box-shadow: 5px 5px 5px;
-  }
+* {
+  box-shadow: 5px 5px 5px;
+}
 
-  .fade-enter,
-  .fade-leave-active {
-    opacity: 0;
-  }
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+}
 
-  .fade-enter-active,
-  .fade-leave-active {
-    transition: opacity .5s;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
 </style>
---
+++ src/components/shared/panel/home/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1 class="center">{{ title }}</h1>
+    <input
+      @input="search = $event.target.value"
+      type="search"
+      class="d-block w-100"
+      placeholder="search..."
+    />
+    <ul class="list">
+      <li class="d-inline" v-for="photo of filtredItems" :key="photo._id">
+        <vue-panel :title="photo.titulo">
+          <img class="w-100" :src="photo.url" :alt="photo.titulo" />
+        </vue-panel>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Panel from "../shared/panel/Panel.vue";
+export default {
+  name: "app",
+  components: {
+    "vue-panel": Panel
+  },
+  computed: {
+    filtredItems() {
+      if (this.search) {
+        //this 'i' it is for to get all, uppercase or not, forgot the sensitive case
+        let filter = new RegExp(this.search.trim(), "i");
+        return this.photos.filter(photo => filter.test(photo.titulo));
+      } else {
+        return this.photos;
+      }
+    }
+  },
+  data() {
+    return {
+      title: "Welcome to Vue Photo",
+      photos: [],
+      search: ""
+    };
+  },
+  created() {
+    this.$http
+      .get("http://localhost:3000/v1/fotos")
+      .then(res => res.json())
+      .then(
+        photos => (this.photos = photos),
+        err => console.error(err)
+      );
+  }
+};
+</script>
+
+<style>
+</style>
