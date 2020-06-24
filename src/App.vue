9929538956b3759d2b99b83d8b68cdadd6f8a130
raloliver <contato@raloliver.com.br>
2020-06-24 11:53:37 +0100

    List photos from API

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,30 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <img :src="image.src" :alt="image.alt">
+    <ul>
+      <li v-for="photo of photos" :key="photo._id">
+        <img :src="photo.url" :alt="photo.titulo" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data() {
-      return {
-        title: 'Welcome to Vue Photo',
-        photos: [
-
-        ]
-      }
-    },
-    created() {
-      
-    }
+export default {
+  name: "app",
+  data() {
+    return {
+      title: "Welcome to Vue Photo",
+      photos: []
+    };
+  },
+  created() {
+    let promise = this.$http.get("http://localhost:3000/v1/fotos");
+    promise.then(res => {
+      res.json().then(photos => (this.photos = photos));
+    });
   }
+};
 </script>
 
 <style lang="scss">
